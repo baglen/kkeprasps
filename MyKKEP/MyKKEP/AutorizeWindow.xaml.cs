1b39a727b54e5de99372444a1e71b1252a1f6b17
zhurako <zhuravl_den@mail.ru>
2020-12-01 18:58:53 +0300

    //данил

---
+++ MyKKEP/MyKKEP/AutorizeWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyKKEP
+{
+    public partial class AutorizeWindow : Page
+    {
+        private static string Token=null;
+        public AutorizeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+                string login = TxtLogin.Text;
+                string password = TxtPassword.Password;
+                string response = Request.PostLogin("https://my.kkep.ru/api.php?", login, password);
+                JObject jObject = JObject.Parse(response);
+                Token = jObject["ses_token"].ToString();
+                MessageBox.Show("Вы успешно авторизованы");
+                Manager.MainFrame.Navigate(new Menu());
+        }
+    }
+}
