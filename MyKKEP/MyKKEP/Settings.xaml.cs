97033a32d074db6d34068a26824fcb5acfcaa333
zhurako <zhuravl_den@mail.ru>
2020-12-05 20:42:28 +0300

    /

--- MyKKEP/MyKKEP/Settings.xaml.cs
+++ MyKKEP/MyKKEP/Settings.xaml.cs
@@ -30,7 +30,7 @@ namespace MyKKEP
             //styleBox.ItemsSource = styles;
            // styleBox.SelectedItem = "dark";
         }
-        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+     /*   private void ThemeChange(object sender, SelectionChangedEventArgs e)
         {
             string style = styleBox.SelectedItem as string;
             // определяем путь к файлу ресурсов
@@ -41,7 +41,7 @@ namespace MyKKEP
             Application.Current.Resources.Clear();
             // добавляем загруженный словарь ресурсов
             Application.Current.Resources.MergedDictionaries.Add(resourceDict);
-        }
+        }*/
 
     }
 }
