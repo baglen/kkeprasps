196e62b863f66895c2c2b7d937aa76bde6e87f53
baglen <baglenvp@gmail.com>
2020-12-05 23:38:35 +0300

    groupList

--- MyKKEP/MyKKEP/Rasp.xaml.cs
+++ MyKKEP/MyKKEP/Rasp.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +21,24 @@ namespace MyKKEP
     /// </summary>
     public partial class Rasp : Page
     {
-        public Rasp()
+        private static string Token;
+        private static string GroupName;
+        public Rasp(string token, string groupName)
         {
             InitializeComponent();
-
+            Token = token;
+            GroupName = groupName;
+            if (GroupName != null)
+            {
+                ComboBoxGroups.SelectedItem = GroupName;
+            }
+            string response = Request.GetGroupList(Token);
+            JArray jArray = JArray.Parse(response);
+            for (int i = 0; i < jArray.Count; i++)
+            {
+                JObject jObject = JObject.Parse(jArray[i].ToString());
+                ComboBoxGroups.Items.Add(jObject["group_name"].ToString());
+            }
         }
     }
 }
