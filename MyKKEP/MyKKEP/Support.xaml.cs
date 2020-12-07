5707e662b1007b952a44403863866842cebae356
Farvolaeth <56363752+Farvolaeth@users.noreply.github.com>
2020-12-07 18:38:43 +0300

    nice boobs ;D

--- MyKKEP/MyKKEP/Support.xaml.cs
+++ MyKKEP/MyKKEP/Support.xaml.cs
@@ -12,25 +12,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using System.Net.Mail;
+
 
 namespace MyKKEP
 {
     /// <summary>
+    /// login my.kkep.app@gmail.com
+    /// pass putin5551
     /// Логика взаимодействия для Support.xaml
     /// </summary>
     public partial class Support : Page
     {
         private string NameUser;
         private string SurnameUser;
-        public Support(string name, string surname)
+        private static string GroupName;
+        public Support(string name, string surname, string groupName)
         {
             InitializeComponent();
             if (name != null && surname != null)
             {
                 NameUser = name;
                 SurnameUser = surname;
+                GroupName = groupName;
             }
             TEST.Text = NameUser + " " + SurnameUser;
+
         }
     }
 }
