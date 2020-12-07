ba6f6e2d2645b73444b667e2ca56c2513656ce60
baglen <baglenvp@gmail.com>
2020-12-07 22:07:19 +0300

    agh

---
+++ MyKKEP/MyKKEP/Days.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyKKEP
+{
+    class Days
+    {
+        public string pairNum { get; set; }
+        //public string pairTime;
+        public string pairSubject { get; set; }
+        public string pairTeacher { get; set; }
+        public string pairAud { get; set; }
+
+    }
+}
